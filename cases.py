4e0097a8a7b4ef697554a36dd366b092ed9f3b11
Kyle <kylereaves@gmail.com>
2022-07-14 20:44:55 -0400

    Adding new cases...updating others

---
+++ cases.py
@@ -0,0 +1,22 @@
+cases = {
+    # We can't assume that the body of these documents is consistent from historic cases to the more recent
+    'dred_scott': 'https://www.law.cornell.edu/supremecourt/text/60/393',
+    'heller': 'https://www.law.cornell.edu/supremecourt/text/07-290#writing-ZS',
+    # Moreover, we can't assume that the dissent is always its accurately tagged class  "dissent"
+    # For example, Scalia wrote Lawrence's dissent in which Roberts and Thomas joined…
+    # …Cornell's website makes it seem like Scalia wrote an opinion
+    'lawrence': 'https://www.law.cornell.edu/supremecourt/text/02-102#writing-ZS',
+    'dobbs': 'https://www.law.cornell.edu/supremecourt/text/19-1392',
+    'nra_v_bruen': 'https://www.law.cornell.edu/supremecourt/text/20-843',
+    'plessy': 'https://www.law.cornell.edu/supremecourt/text/163/537',
+    'citizens_united': 'https://www.law.cornell.edu/supremecourt/text/08-205,#writing-ZS',
+    'west_virginia_v_epa': 'https://www.law.cornell.edu/supremecourt/text/20-1530',
+    'shelby': 'https://www.law.cornell.edu/supremecourt/text/12-96',
+    'roe': 'https://www.law.cornell.edu/supremecourt/text/410/113',
+    'casey': 'https://www.law.cornell.edu/supremecourt/text/505/833',
+    'griswold': 'https://www.law.cornell.edu/supremecourt/text/381/479',
+    'lochner': 'https://www.law.cornell.edu/supremecourt/text/198/45',
+    'korematsu': 'https://www.law.cornell.edu/supremecourt/text/323/214',
+    'chevron': 'https://www.law.cornell.edu/supremecourt/text/467/837',
+    'brnovich': 'https://www.law.cornell.edu/supremecourt/text/19-1257',
+}
